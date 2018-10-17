2c6d9e706385a230b0653e91a525909f5555384f
Scott Bakker <scott@bakkergames.com>
2018-10-17 17:48:51 -0400

    adding healing

--- items.py
+++ items.py
@@ -5,11 +5,13 @@ class Weapon:
     def __str__(self):
         return self.name
 
+
 class Rock(Weapon):
     def __init__(self):
         self.name = "Rock"
         self.description = "A fist-sized rock, suitable for bludgeoning."
         self.damage = 5
+
 
 class Dagger(Weapon):
     def __init__(self):
@@ -18,10 +20,25 @@ class Dagger(Weapon):
                 "Somewhat more dangerous than a rock."
         self.damage = 10
 
+
 class RustySword:
     def __init__(self):
         self.name = "Rusty sword"
         self.description = "This sword is showing its age, " \
                 "but still has some fight in it."
         self.damage = 20
+
+
+class Consumable:
+    def __init__(self):
+        raise NotImplementedError("Do not create raw Consumable objects.")
+
+    def __str__(self):
+        return "{} (+{} HP)".format(self.name, self.healing_value)
+
+
+class CrustyBread(Consumable):
+    def __init__(self):
+        self.name = "Crusty Bread"
+        self.healing_value = 10
 
